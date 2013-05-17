6da51b74d299adb03f9f4cd3de005320e529e718
David Henderson <cayuse@samba.(none)>
2013-05-17 08:40:44 -0700

    added separate rgb setters

--- vTreeLEDCmdProcessor.cpp
+++ vTreeLEDCmdProcessor.cpp
@@ -30,7 +30,8 @@ void vTreeLEDCmdProcessor::Loop()
 //This command responds always and with the following information. It should also include address.
 //But i have to go so I can't do that right atm.
             if (strcmp(pCmd,"status") == 0) {
-                  sprintf(buffer,"Ok:Red:%d Green:%d Blue:%d Intensity:%d\n",
+                  sprintf(buffer,"Ok:Address: %d Red:%d Green:%d Blue:%d Intensity:%d\n",
+                    _pPC->myAddr,
                     _pPC->redValue,
                     _pPC->greenValue,
                     _pPC->blueValue,
@@ -62,7 +63,59 @@ void vTreeLEDCmdProcessor::Loop()
                 } else {
                     _pHW->print("Fail:setRGB Requires an address plus 3 values 0-255\n");
                 }
+// This command sets Red value
+// This command is accepted by address or broadcast.
+// This command should have a short command name.
+// This command does not talk back.
 
+
+            } else if(strcmp(pCmd,"setRed") == 0) {
+                uint8_t address = 0;
+                int redValue = -1;
+                if (paramCnt() == 2) {
+                    getParam(0,address);
+                    getParam(1,redValue);
+                    if ( _pPC->IsMyOrBcast(address)){
+                        _pPC->setRedValue(redValue);
+                    }
+                } else {
+                    //_pHW->print("Fail:\n");
+                }
+// This command sets Green value
+// This command is accepted by address or broadcast.
+// This command should have a short command name.
+// This command does not talk back.
+
+            } else if(strcmp(pCmd,"setGreen") == 0) {
+                uint8_t address = 0;
+                int greenValue = -1;
+                if (paramCnt() == 2) {
+                    getParam(0,address);
+                    getParam(2,greenValue);
+                    getParam(3,blueValue);
+                    if ( _pPC->IsMyOrBcast(address)){
+                        _pPC->setGreenValue(greenValue);
+                    }
+                } else {
+                    //_pHW->print("Fail:\n");
+                }
+// This command sets Blue value
+// This command is accepted by address or broadcast.
+// This command should have a short command name.
+// This command does not talk back.
+
+            } else if(strcmp(pCmd,"setBlue") == 0) {
+                uint8_t address = 0;
+                int blueValue = -1;
+                if (paramCnt() == 2) {
+                    getParam(0,address);
+                    getParam(3,blueValue);
+                    if ( _pPC->IsMyOrBcast(address)){
+                        _pPC->setBlueValue(blueValue);
+                    }
+                } else {
+                    //_pHW->print("Fail:\n");
+                }
 // This command sets the programmable Address
 // This command is accepted by address or broadcast.
 // This command does not require a short command name.
